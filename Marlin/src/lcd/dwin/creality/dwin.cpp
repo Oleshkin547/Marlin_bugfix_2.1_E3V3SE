33d4f32c0a6e1df1c617429648c7ee076541facc
Oleshkin547 <62075824+Oleshkin547@users.noreply.github.com>
2026-03-16 18:51:44 +0300

    Update dwin.cpp

--- Marlin/src/lcd/dwin/creality/dwin.cpp
+++ Marlin/src/lcd/dwin/creality/dwin.cpp
@@ -8040,7 +8040,7 @@ void HMI_Control()
       Draw_Temperature_Menu();
       break;
       case CONTROL_CASE_MOVE: // Motion
-      checkkey = (long)Motion;
+     checkkey = (HMI_CheckKey)2;
       select_motion.reset();
       Draw_Motion_Menu();
       break;
