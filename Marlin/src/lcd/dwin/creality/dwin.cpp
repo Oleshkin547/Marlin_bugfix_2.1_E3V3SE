a312b9c4108950ecdf826c7a7339146e429b612a
Oleshkin547 <62075824+Oleshkin547@users.noreply.github.com>
2026-03-16 18:42:43 +0300

    Update dwin.cpp

--- Marlin/src/lcd/dwin/creality/dwin.cpp
+++ Marlin/src/lcd/dwin/creality/dwin.cpp
@@ -7649,7 +7649,7 @@ void HMI_Prepare()
       DWIN_Draw_Signed_Float(font8x16, Color_Bg_Black, 3, UNITFDIGITS, VALUERANGE_X, MBASE(2), current_position.y * MINUNITMULT);
       DWIN_Draw_Signed_Float(font8x16, Color_Bg_Black, 3, UNITFDIGITS, VALUERANGE_X, MBASE(3), current_position.z * MINUNITMULT);
 #if HAS_HOTEND
-      HMI_ValueStruct.Move_E_scaled = current_position.e_view() * MINUNITMULT;
+      HMI_ValueStruct.Move_E_scaled = current_position[E_AXIS] * MINUNITMULT;
       HMI_ValueStruct.Move_E_scaled = current_position[E_AXIS] * MINUNITMULT;
 #endif
 
